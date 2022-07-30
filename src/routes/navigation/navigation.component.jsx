460b7d66aaad47eefd1b63fffb553ed6809cffd7
“ChillYao” <51960333+ChillYao@users.noreply.github.com>
2022-07-30 11:28:42 +0200

    Update the style for navigation bar

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,17 +1,20 @@
 import { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import './navigation.component.scss';
 
 const Navigation = () => {
     return (
         <Fragment>
             <div className="navigation">
                 <Link className="logo-container" to="/">
-                    <div>Logo</div>
+                    <CrwnLogo className="logo" />
                 </Link>
-                <Link className="nav-link" to="/shop">
-                    <div>Shop</div>
-                </Link>
-                <h1>This is Navigation</h1>
+                <div className="nav-links-container">
+                    <Link className="nav-link" to="/shop">
+                        <div>Shop</div>
+                    </Link>
+                </div>
             </div>
             <Outlet />
         </Fragment>

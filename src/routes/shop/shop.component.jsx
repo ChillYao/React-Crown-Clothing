de3fe3de10c5cf5a96a8aaa8b33ee7f49baed719
“ChillYao” <51960333+ChillYao@users.noreply.github.com>
2022-08-16 16:12:01 +0200

    update the shop page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useContext } from 'react';
 import { ProductsContext } from '../../contexts/products.context';
+import ProductCard from '../../components/product-card/product-card.component';
+import './shop.style.scss';
 
 const Shop = () => {
     const { products } = useContext(ProductsContext);
-    return products.map(({ id, name, imageUrl, price }) => {
-        return (
-            <div key={id}>
-                <h1>{name}</h1>
-            </div>
-        );
-    });
+    return (
+        <div className="products-container">
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+            ))}
+        </div>
+    );
 };
 
 export default Shop;

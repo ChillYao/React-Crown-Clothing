ad9290a1e31a7fe89b25c0dd2aaa57fb14d99248
tong.yao <tong.yao@check24.de>
2022-11-14 17:50:42 +0100

    change back auth domain

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -27,7 +27,7 @@ import {
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: 'AIzaSyDwmhxJHWUAokm_EPZtxhnP-f0kLv4Zo7U',
-    authDomain: 'fluffy-swan-8342cc.netlify.app',
+    authDomain: 'crwn-cloting-db-9352f.firebaseapp.com',
     projectId: 'crwn-cloting-db-9352f',
     storageBucket: 'crwn-cloting-db-9352f.appspot.com',
     messagingSenderId: '891071286606',

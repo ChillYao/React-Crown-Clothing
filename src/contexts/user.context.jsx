be968d166195f0a48041e949247e5b3f297cdaf1
tong.yao <tong.yao@check24.de>
2022-11-22 09:47:32 +0100

    use Reducer in the user.context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -7,10 +7,31 @@ export const UserContext = createContext({
     setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER',
+  };
+
+const INITIAL_STATE = {
+    currentUser: null,
+};
+
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+  
+    switch (type) {
+      case USER_ACTION_TYPES.SET_CURRENT_USER:
+        return { ...state, currentUser: payload };
+      default:
+        throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+  };
+
 //The user provider for different component
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const value = { currentUser, setCurrentUser };
+    const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+    const setCurrentUser = (user) =>
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, currentUser: user });
 
     useEffect(() => {
         const unsubscribe = onAuthStateChangedListner((user) => {
@@ -22,6 +43,10 @@ export const UserProvider = ({ children }) => {
 
         return unsubscribe;
     }, []);
+
+    const value = {
+        currentUser,
+      };
 
     return (
         <UserContext.Provider value={value}>{children}</UserContext.Provider>

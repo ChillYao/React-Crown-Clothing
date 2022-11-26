b92184b752e9a6185ac895534958f3d4540a3b10
“ChillYao” <51960333+ChillYao@users.noreply.github.com>
2022-11-26 15:15:42 +0100

    refactor a bit context

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -8,24 +8,7 @@ export const UserContext = createContext({
     setCurrentUser: () => null,
 });
 
-export const USER_ACTION_TYPES = {
-    SET_CURRENT_USER: 'SET_CURRENT_USER',
-  };
 
-const INITIAL_STATE = {
-    currentUser: null,
-};
-
-const userReducer = (state, action) => {
-    const { type, payload } = action;
-  
-    switch (type) {
-      case USER_ACTION_TYPES.SET_CURRENT_USER:
-        return { ...state, currentUser: payload };
-      default:
-        throw new Error(`Unhandled type ${type} in userReducer`);
-    }
-  };
 
 //The user provider for different component
 export const UserProvider = ({ children }) => {

5a6d643de00fac64d0fb4506caf1349bfc82419e
“ChillYao” <51960333+ChillYao@users.noreply.github.com>
2022-11-27 14:15:25 +0100

    finish user redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { createUserDocumentFromAuth, onAuthStateChangedListner } from "./utils/firebase/firebase.utils"
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Auth from './routes/auth/auth.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './components/checkout/checkout.component';
-
+import { setCurrentUser } from './store/user/user.action';
+import { useDispatch } from 'react-redux';
 const App = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedListner((user) => {
+            if (user){
+                createUserDocumentFromAuth(user);
+            }
+            dispatch(setCurrentUser(user));
+        });
+
+        return unsubscribe;
+    }, [dispatch]);
     return (
         <Routes>
             <Route path="/" element={<Navigation />}>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,10 @@
-import { Fragment, useContext, useState } from 'react';
+import { Fragment, useContext } from 'react';
 import { Outlet } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
-import { UserContext } from '../../contexts/user.context';
 import { CartContext } from '../../contexts/cart.context';
+import { selectCurrentUser } from '../../store/user/user.selector';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import {
     NavigationContainer,
@@ -12,10 +12,12 @@ import {
     NavLinks,
     NavLink,
 } from './navigation.style';
+import { useSelector } from 'react-redux';
 
 const Navigation = () => {
-    const { currentUser } = useContext(UserContext);
+    const currentUser = useSelector (selectCurrentUser);
     const { isCartOpen } = useContext(CartContext);
+
 
     return (
         <Fragment>

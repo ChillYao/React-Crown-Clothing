65e6db3d6953e89c00feb80911a3f5760a8844f4
tong.yao <tong.yao@check24.de>
2024-07-22 09:41:58 +0200

    update firebase configuration

---
+++ src/utils/firebase/firebase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDwmhxJHWUAokm_EPZtxhnP-f0kLv4Zo7U',
+  authDomain: 'crwn-cloting-db-9352f.firebaseapp.com',
+  projectId: 'crwn-cloting-db-9352f',
+  storageBucket: 'crwn-cloting-db-9352f.appspot.com',
+  messagingSenderId: '891071286606',
+  appId: '1:891071286606:web:4f814ab27214570bc2e21f',
+};
